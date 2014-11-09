39eecadf1b563c3cad1fe9ab8943b8a3bd2b2686
boekabart <bartdb@gmail.com>
2014-11-09 22:52:52 +0100

    Rename

--- Bootstrapper.cs
+++ Bootstrapper.cs
@@ -3,7 +3,7 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Threading;
 
-namespace RunnableServiceBootstrap
+namespace ServiceBootstrap
 {
     public static class Bootstrapper
     {
--- ServiceHelper.cs
+++ ServiceHelper.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Configuration.Install;
 using System.ServiceProcess;
 
-namespace RunnableServiceBootstrap
+namespace ServiceBootstrap
 {
     internal class ServiceHelper
     {
